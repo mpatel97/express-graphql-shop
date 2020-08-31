52b4aaece37a45bb7c1ac2efdf5b37208d4c1529
Mihir Patel <mihir.patel@alphacertlabs.com>
2020-09-01 00:50:33 +1200

    Basic GraphQL setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,43 @@
-console.log("huh?");
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+
+const main = async () => {
+  const app = express();
+
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema: buildSchema(`
+        type RootQuery {
+            posts: [String!]!
+        }
+
+        type RootMutation {
+            createPost(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+      rootValue: {
+        posts: () => {
+          return ["hello", "guys", "teehee"];
+        },
+      },
+      graphiql: true,
+    })
+  );
+
+  app.get("/", (_req, res) => {
+    res.send("Hello World");
+  });
+
+  app.listen(3000);
+};
+
+main().catch((err) => {
+  console.log(err);
+});
